b398f0ca1318665308dc1f8ddc2d7b5553c21e72
somang1206 <dlthakd1206@naver.com>
2022-10-17 13:56:52 +0900

    sm_code_home

--- js/menu_scroll.js
+++ js/menu_scroll.js
@@ -12,3 +12,46 @@ $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 
 
 //메뉴 클릭시 자연스러운 스크롤 이동
+
+
+var burger = $('.menu-trigger');
+
+burger.each(function(index){
+  var $this = $(this);
+
+  $this.on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('active-' + (index+1));
+  })
+});
+
+var h = 0;
+
+$(burger).on("click", function () {
+if (h == 0) {
+$('.m_menu').animate({
+right: '0',
+opacity: 1
+}, 500);
+$(this).addClass('active-1');
+h++;
+} else if (h == 1) {
+$('.m_menu').animate({
+right: '-20%',
+/* opacity: 0 */
+}, 500);
+$(this).removeClass('active-1');
+h--;
+}
+$(window).scroll(function() {
+sct = $(window).scrollTop();
+if(sct>30){
+$('.m_menu').css({
+opacity: 0,
+right :'-20%'
+}, 500);
+$(burger).removeClass('active-1');
+h=0;
+}
+});
+})
